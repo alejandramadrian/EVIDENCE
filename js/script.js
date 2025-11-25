a52078bdce4a02bff62ef20595f0fdf952989c2c
Alejandra <alejandramadrian@gmail.com>
2025-11-24 23:24:04 -0300

    Avance contenido, funcionalidad y diseño de nuevas escenas

--- js/script.js
+++ js/script.js
@@ -34,3 +34,78 @@ if (btnInicio) {
         window.location.href = '../index.html'
     });
 }
+
+
+const btnIvan = document.getElementById ('btnIvan');
+ 
+if (btnIvan) {
+    btnIvan.addEventListener('click', function (){
+
+        window.location.href = '../ivan/ivan.html'
+    });
+}
+
+const btnVera = document.getElementById ('btnVera');
+ 
+if (btnVera) {
+    btnVera.addEventListener('click', function (){
+
+        window.location.href = '../vera/vera.html'
+    });
+}
+
+
+const btnVolver = document.getElementById ('btnVolver');
+ 
+if (btnVolver) {
+    btnVolver.addEventListener('click', function (){
+
+        window.location.href = '../seleccion/seleccion.html'
+    });
+}
+
+const btnElIvan = document.getElementById ('btnElIvan');
+ 
+if (btnElIvan) {
+    btnElIvan.addEventListener('click', function (){
+
+        window.location.href = '../escenas/ivan-1.html'
+    });
+}
+
+const btnElVera = document.getElementById ('btnElVera');
+ 
+if (btnElVera) {
+    btnElVera.addEventListener('click', function (){
+
+        window.location.href = '../escenas/vera-1.html'
+    });
+}
+
+
+const btnVInv = document.getElementById ('btnVInv');
+ 
+if (btnVInv) {
+    btnVInv.addEventListener('click', function (){
+
+        window.location.href = '../escenas/vera-2.html'
+    });
+}
+
+const btnIInv = document.getElementById ('btnIInv');
+
+if (btnIInv) {
+    btnIInv.addEventListener('click', function (){
+
+        window.location.href = '../escenas/ivan-2.html'
+    });
+}
+
+const btnVEnc = document.getElementById ('btnVEnc');
+ 
+if (btnVEnc) {
+    btnVEnc.addEventListener('click', function (){
+
+        window.location.href = '../escenas/encuentro.html'
+    });
+}
